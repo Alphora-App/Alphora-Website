ae2b692ac001a7b27906509180eb18691053595e
Jared Beresford <jaredberesford@gmail.com>
2025-10-15 23:22:28 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Serving at https://www.alphora.app -> base must be '/'
 export default defineConfig({
   plugins: [react()],
-})

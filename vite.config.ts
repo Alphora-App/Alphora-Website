711d200372b292dcd11a6144731717b28494d8a4
jsberesford <jaredberesford@gmail.com>
2025-10-16 21:59:56 -0400

    Moved files
    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Serving at https://www.alphora.app -> base must be '/'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // IMPORTANT
+  base: '/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+  },
+  server: {
+    port: 5173,
+    open: true,
+  },
 })
